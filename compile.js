b0ffdafbf51eb5c44fc1d66296fe4de8c8c5b602
Jurvis Tan <jurvis.tan@me.com>
2020-08-06 11:39:47 -0700

    spike compile script

---
+++ compile.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const fs = require('fs-extra');
+const solc = require('solc');
+
+const buildPath = path.resolve(__dirname, 'build')
+fs.removeSync(buildPath);
+
+const storyPath = path.resolve(__dirname, 'contracts', 'Story.sol');
+const source = fs.readFileSync(storyPath, 'utf8');
+
+const input = {
+  language: 'Solidity',
+  sources: {
+    'Story.sol' : {
+      content: source
+    }
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': [ '*' ]
+      }
+    }
+  }
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts;
+
+fs.ensureDirSync(buildPath)
+
+for (let contract in output['Story.sol']) {
+  fs.outputJSONSync(
+    path.resolve(buildPath, `${contract}.json`),
+    output['Story.sol'][contract]
+  );
+}
