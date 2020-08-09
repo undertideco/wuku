ab6044e14bcda05431b55c48b1d21e450f5c2549
Jurvis Tan <jurvis.tan@me.com>
2020-08-08 19:28:39 -0700

    load stories in getInitialProps of home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import factory from '../ethereum/factory';
 import Layout from '../components/Layout';
 
 class StoryIndex extends Component {
+  static async getInitialProps() {
+    const stories = await factory.methods.getDeployedStories().call();
+
+    return { stories };
+  }
+
   render() {
     return (
       <Layout>
