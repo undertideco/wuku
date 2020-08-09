41d3592a4aeff55d87b75061490e5186703a5ecc
Jurvis Tan <jurvis.tan@me.com>
2020-08-08 19:08:27 -0700

    spike index page

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes';
+
+export default () => {
+  return (
+    <Menu style={{ marginTop: '10px' }}>
+    <Link route='/'>
+      <a className="item">
+        Wuku
+      </a>
+    </Link>
+
+    <Menu.Menu position="right">
+      <Link route="/">
+        <a className="item">
+          Stories
+        </a>
+      </Link>
+
+      <Link route="/">
+        <a className="item">
+          +
+        </a>
+      </Link>
+    </Menu.Menu>
+  </Menu>
+  );
+};
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from '../components/Layout';
+
+class StoryIndex extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>Hello World</h1>
+      </Layout>
+    )
+  }
+}
+
+export default StoryIndex;
