0438b6c6febd53da5fcb399e4d117e1cd5305ecc
Jurvis Tan <jurvis.tan@me.com>
2020-08-10 22:22:38 -0700

    expose factor contract address to browser

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import StoryFactory from './build/StoryFactory.json';
 
 const instance = new web3.eth.Contract(
   StoryFactory.abi,
-  process.env.STORYFACTORY_CONTRACT_ADDRESS
+  process.env.NEXT_PUBLIC_STORYFACTORY_CONTRACT_ADDRESS
 );
 
 export default instance;
