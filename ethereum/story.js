52fd0889341558866ebc19f170b40919681abfbf
Jurvis Tan <jurvis.tan@me.com>
2020-08-27 14:06:02 -0700

    fix next.js fast reload warning

--- ethereum/story.js
+++ ethereum/story.js
@@ -1,6 +1,8 @@
 import web3 from './web3';
 import Story from './build/Story.json'
 
-export default address => {
+const story = address => {
   return new web3.eth.Contract(Story.abi, address);
 }
+
+export default story;
