cba80aacdbf6e895d0ff26fa17f8be1fffcb0646
Jurvis Tan <jurvis.tan@me.com>
2020-09-03 16:28:34 -0700

    run prettier on test suite

--- test/Story.test.js
+++ test/Story.test.js
@@ -12,6 +12,9 @@ let factory;
 let storyAddress;
 let story;
 
+const minStartingContribution = 13000000000000000;
+const baseContributionValue = 250000000000000;
+
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   factory = await new web3.eth.Contract(compiledFactory.abi)
@@ -21,14 +24,11 @@ beforeEach(async () => {
   await factory.methods.createStory('test story').send({
     from: accounts[0],
     gas: '3000000',
-    value: '13000000000000000'
+    value: minStartingContribution.toString(),
   });
 
   [storyAddress] = await factory.methods.getDeployedStories().call();
-  story = await new web3.eth.Contract(
-    compiledStory.abi,
-    storyAddress
-  );
+  story = await new web3.eth.Contract(compiledStory.abi, storyAddress);
 });
 
 describe('Stories and Contributions', () => {
@@ -42,39 +42,43 @@ describe('Stories and Contributions', () => {
     assert.equal(accounts[0], host);
   });
 
-  it('marks host as eligible voter', async() => {
+  it('marks host as eligible voter', async () => {
     const approvedVoter = await story.methods.voters(accounts[0]).call();
 
     assert(approvedVoter);
   });
 
-  it('marks initial contribution as highest voted', async() => {
-    const highestVotedContribution = await story.methods.getHighestVotedContribution().call();
+  it('marks initial contribution as highest voted', async () => {
+    const highestVotedContribution = await story.methods
+      .getHighestVotedContribution()
+      .call();
 
-    assert.equal(highestVotedContribution.description, 'test story')
+    assert.equal(highestVotedContribution.description, 'test story');
   });
 
-  it("allow people to make story contribution, increase the minimum contribution", async () => {
+  it('allow people to make story contribution, increase the minimum contribution', async () => {
     await story.methods.createContribution('first contribution').send({
       value: '250000000000000',
       from: accounts[1],
-      gas: '3000000'
+      gas: '3000000',
     });
 
     const minContribution = await story.methods.getMinimumContribution().call();
     const contribution = await story.methods.contributions(1).call();
-    const contributionsCount = await story.methods.getContributionsCount().call();
+    const contributionsCount = await story.methods
+      .getContributionsCount()
+      .call();
     const voterApproved = await story.methods.voters(accounts[1]).call();
 
     assert(voterApproved);
     assert.equal(parseInt(contributionsCount, 10), 2);
     assert.equal(contribution.contributor, accounts[1]);
-    assert.equal(minContribution, '500000000000000')
+    assert.equal(minContribution, '500000000000000');
   });
 
-  it('sets current block time as created time', async() => {
+  it('sets current block time as created time', async () => {
     const createdTime = await story.methods.createdTime().call();
-    const latestBlock = await web3.eth.getBlock('latest')
+    const latestBlock = await web3.eth.getBlock('latest');
 
     assert.equal(createdTime, latestBlock.timestamp);
   });
@@ -83,29 +87,34 @@ describe('Stories and Contributions', () => {
     await story.methods.createContribution('first contribution').send({
       value: '250000000000000',
       from: accounts[1],
-      gas: '3000000'
+      gas: '3000000',
     });
- 
+
     const isContributor = await story.methods.isContributor(accounts[1]).call();
-    assert(isContributor)
-  })
+    assert(isContributor);
+  });
 });
 
-describe("Get Summary", () => {
-  it("get summary", async () => {
+describe('Get Summary', () => {
+  it('get summary', async () => {
+    await story.methods.createContribution('first contribution').send({
+      value: '250000000000000',
+      from: accounts[1],
+      gas: '3000000',
+    });
     const summary = await story.methods.getSummary().call();
 
-    assert.equal(summary[0].length, 1)
-    assert.equal(summary[1], '250000000000000')
-    assert.equal(summary[2], accounts[0])
-  })
-})
+    assert.equal(summary[0].length, 2);
+    assert.equal(summary[1], (baseContributionValue * 2).toString());
+    assert.equal(summary[2], accounts[0]);
+  });
+});
 
-describe("Voting", () => {
-  it("do not allow non-contributors to vote", async() => {
+describe('Voting', () => {
+  it('do not allow non-contributors to vote', async () => {
     try {
       await story.methods.voteContribution(0).send({
-        from: accounts[1]
+        from: accounts[1],
       });
       assert(false);
     } catch (err) {
@@ -113,43 +122,45 @@ describe("Voting", () => {
     }
   });
 
-  it('allow contributors to vote and change highest voted contribution index', async() => {
-    const contributionString = "first contribution"
+  it('allow contributors to vote and change highest voted contribution index', async () => {
+    const contributionString = 'first contribution';
 
     await story.methods.createContribution(contributionString).send({
       value: '250000000000000',
       from: accounts[1],
-      gas: '3000000'
+      gas: '3000000',
     });
 
     await story.methods.voteContribution(1).send({
       from: accounts[0],
-      gas: '3000000'
+      gas: '3000000',
     });
 
     const voteCount = await story.methods.getContributionVoteCount(1).call();
-    const highestVotedContribution = await story.methods.getHighestVotedContribution().call();
-     
+    const highestVotedContribution = await story.methods
+      .getHighestVotedContribution()
+      .call();
+
     assert.equal(voteCount, 1);
     assert.equal(highestVotedContribution.description, contributionString);
   });
 
-  it('do not allow users to vote for their own contribution', async() => {
-    const errorString = "User not allowed to vote for their own contribution";
-    await story.methods.createContribution("some contribution").send({
+  it('do not allow users to vote for their own contribution', async () => {
+    const errorString = 'User not allowed to vote for their own contribution';
+    await story.methods.createContribution('some contribution').send({
       value: '250000000000000',
       from: accounts[1],
-      gas: '3000000'
-    })
+      gas: '3000000',
+    });
 
     try {
       await story.methods.voteContribution(1).send({
         from: accounts[1],
-        gas: '3000000'
-      })
+        gas: '3000000',
+      });
       assert(false);
-    } catch(err) {
-      const errReason = Object.values(err["results"])[0]["reason"];
+    } catch (err) {
+      const errReason = Object.values(err['results'])[0]['reason'];
 
       assert(err);
       assert.equal(errReason, errorString);
@@ -159,37 +170,37 @@ describe("Voting", () => {
 
 const SECONDS_IN_DAY = 86400;
 
-describe("Expire Contributions", () => {
-  it("close story from contributions after 3 days", async() => {
-    await advanceTimeAndBlock(web3, SECONDS_IN_DAY * 3)
+describe('Expire Contributions', () => {
+  it('close story from contributions after 3 days', async () => {
+    await advanceTimeAndBlock(web3, SECONDS_IN_DAY * 3);
 
     const hasClosed = await story.methods.hasClosedForContributions().call();
 
     assert(hasClosed);
   });
 
-  it("close story from voting after 4 days", async() => {
-    await advanceTimeAndBlock(web3, SECONDS_IN_DAY * 4)
+  it('close story from voting after 4 days', async () => {
+    await advanceTimeAndBlock(web3, SECONDS_IN_DAY * 4);
 
     const hasClosed = await story.methods.hasClosedForVoting().call();
 
-    assert(hasClosed); 
-  })
+    assert(hasClosed);
+  });
 });
 
 describe('Finalize Story Contract', () => {
-  it('finalize story and distribute winnings', async() => {
-    const contributionString = "first contribution";
+  it('finalize story and distribute winnings', async () => {
+    const contributionString = 'first contribution';
 
     await story.methods.createContribution(contributionString).send({
       value: '250000000000000',
       from: accounts[1],
-      gas: '3000000'
+      gas: '3000000',
     });
 
     await story.methods.voteContribution(1).send({
       from: accounts[0],
-      gas: '3000000'
+      gas: '3000000',
     });
 
     const totalPrizePool = await web3.eth.getBalance(story.options.address);
@@ -200,38 +211,41 @@ describe('Finalize Story Contract', () => {
 
     try {
       const result = await story.methods.finalize().send({
-        from: accounts[0]
+        from: accounts[0],
       });
       assert(true);
-    } catch(err) {
+    } catch (err) {
       assert(false, err);
     }
 
     const newWinnerWalletBalance = await web3.eth.getBalance(accounts[1]);
-    assert.equal(parseInt(newWinnerWalletBalance, 10), parseInt(winnerWalletBalance) + parseInt(winnerPayout));
+    assert.equal(
+      parseInt(newWinnerWalletBalance, 10),
+      parseInt(winnerWalletBalance) + parseInt(winnerPayout)
+    );
   });
 
   it('only allow story to finalize after voting is closed', async () => {
     try {
       await story.methods.finalize().send({
-        from: accounts[0]
+        from: accounts[0],
       });
       assert(false);
-    } catch(err) {
+    } catch (err) {
       assert(err);
     }
   });
 
   it('only story host should be allowed to finalize story', async () => {
     await advanceTimeAndBlock(web3, SECONDS_IN_DAY * 4);
-    
+
     try {
       await story.methods.finalize().send({
-        from: accounts[1]
+        from: accounts[1],
       });
       assert(false);
-    } catch(err) {
+    } catch (err) {
       assert(err);
     }
   });
-})
+});
