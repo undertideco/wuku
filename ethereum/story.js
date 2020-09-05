a23d282da190aa0a5c9b00a358bd8a9cf0309efc
Jurvis Tan <jurvis.tan@me.com>
2020-09-05 11:50:36 -0700

    run autofix on all files in codebae

--- ethereum/story.js
+++ ethereum/story.js
@@ -1,5 +1,5 @@
-import web3 from './web3';
 import Story from './build/contracts/Story.json';
+import web3 from './web3';
 
 const story = (address) => {
   return new web3.eth.Contract(Story.abi, address);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { createServer } = require('http');
 const next = require('next');
 
 const app = next({
-  dev: process.env.NODE_ENV !== 'production'
+  dev: process.env.NODE_ENV !== 'production',
 });
 
 const routes = require('./routes');
@@ -11,6 +11,6 @@ const handler = routes.getRequestHandler(app);
 app.prepare().then(() => {
   createServer(handler).listen(3000, (err) => {
     if (err) throw err;
-    console.log('Ready on localhost:3000')
+    console.log('Ready on localhost:3000');
   });
 });
