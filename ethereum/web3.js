dfa8e01cec8ff0924326a56143fc311069a561fa
Jurvis Tan <jurvis.tan@me.com>
2020-09-05 11:45:55 -0700

    read provider url from env file

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -7,7 +7,7 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   web3 = new Web3(window.web3.currentProvider);
 } else {
   const provider = new Web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/v3/5e8a0b5f0a1c4ab3ab68a2830dbaf892'
+    process.env.NEXT_PROVIDER_URL
   );
   web3 = new Web3(provider);
 }
